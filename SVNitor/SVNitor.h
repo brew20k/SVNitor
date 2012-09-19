b97fcf5fb81b5cf211fad8535eb19fefb1f9f204
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2012-09-18 20:12:13 -0500

    Adding Project
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

---
+++ SVNitor/SVNitor.h
@@ -0,0 +1,15 @@
+//
+//  SVNitor.h
+//  SVNitor
+//
+//  Created by Letteer Lewis on 9/18/12.
+//  Copyright (c) 2012 Purdue. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface SVNitor : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ SVNitor/SVNitor.m
@@ -0,0 +1,17 @@
+//
+//  SVNitor.m
+//  SVNitor
+//
+//  Created by Letteer Lewis on 9/18/12.
+//  Copyright (c) 2012 Purdue. All rights reserved.
+//
+
+#import "SVNitor.h"
+
+@implementation SVNitor
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
