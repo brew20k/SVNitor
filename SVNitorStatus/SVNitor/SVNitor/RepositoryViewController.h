45e3df78dc4265372b88e03c84a4b6aee2cadd8c
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2012-09-22 14:29:38 -0500

    Adding TableViewController
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

---
+++ SVNitorStatus/SVNitor/SVNitor/RepositoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  RepositoryViewController.h
+//  SVNitor
+//
+//  Created by Kevin Kinnebrew on 9/22/12.
+//  Copyright (c) 2012 Letteer's Home. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RepositoryViewController : NSViewController <NSTableViewDataSource, NSTableViewDelegate>
+
+@end
---
+++ SVNitorStatus/SVNitor/SVNitor/RepositoryViewController.m
@@ -0,0 +1,13 @@
+//
+//  RepositoryViewController.m
+//  SVNitor
+//
+//  Created by Kevin Kinnebrew on 9/22/12.
+//  Copyright (c) 2012 Letteer's Home. All rights reserved.
+//
+
+#import "RepositoryViewController.h"
+
+@implementation RepositoryViewController
+
+@end
